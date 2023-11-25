0144ae452bb63953a8481732da847591fdad1048
Jelmer Pijnappel <chocolatepinecone@gmail.com>
2023-11-25 13:16:50 +0100

    Refactored UUID file to use tabs instead of double spaces to indent

--- uuid-generator/uuid.gd
+++ uuid-generator/uuid.gd
@@ -6,29 +6,29 @@ extends Node
 const BYTE_MASK: int = 0b11111111
 
 static func uuidbin():
-  randomize()
-  # 16 random bytes with the bytes on index 6 and 8 modified
-  return [
+	randomize()
+	# 16 random bytes with the bytes on index 6 and 8 modified
+	return [
 	randi() & BYTE_MASK, randi() & BYTE_MASK, randi() & BYTE_MASK, randi() & BYTE_MASK,
 	randi() & BYTE_MASK, randi() & BYTE_MASK, ((randi() & BYTE_MASK) & 0x0f) | 0x40, randi() & BYTE_MASK,
 	((randi() & BYTE_MASK) & 0x3f) | 0x80, randi() & BYTE_MASK, randi() & BYTE_MASK, randi() & BYTE_MASK,
 	randi() & BYTE_MASK, randi() & BYTE_MASK, randi() & BYTE_MASK, randi() & BYTE_MASK,
-  ]
+	]
 
 static func uuidbinrng(rng: RandomNumberGenerator):
-  rng.randomize()
-  return [
+	rng.randomize()
+	return [
 	rng.randi() & BYTE_MASK, rng.randi() & BYTE_MASK, rng.randi() & BYTE_MASK, rng.randi() & BYTE_MASK,
 	rng.randi() & BYTE_MASK, rng.randi() & BYTE_MASK, ((rng.randi() & BYTE_MASK) & 0x0f) | 0x40, rng.randi() & BYTE_MASK,
 	((rng.randi() & BYTE_MASK) & 0x3f) | 0x80, rng.randi() & BYTE_MASK, rng.randi() & BYTE_MASK, rng.randi() & BYTE_MASK,
 	rng.randi() & BYTE_MASK, rng.randi() & BYTE_MASK, rng.randi() & BYTE_MASK, rng.randi() & BYTE_MASK,
-  ]
+	]
 
 static func v4():
   # 16 random bytes with the bytes on index 6 and 8 modified
-  var b = uuidbin()
+	var b = uuidbin()
 
-  return '%02x%02x%02x%02x-%02x%02x-%02x%02x-%02x%02x-%02x%02x%02x%02x%02x%02x' % [
+	return '%02x%02x%02x%02x-%02x%02x-%02x%02x-%02x%02x-%02x%02x%02x%02x%02x%02x' % [
 	# low
 	b[0], b[1], b[2], b[3],
 
@@ -47,9 +47,9 @@ static func v4():
   
 static func v4_rng(rng: RandomNumberGenerator):
   # 16 random bytes with the bytes on index 6 and 8 modified
-  var b = uuidbinrng(rng)
+	var b = uuidbinrng(rng)
 
-  return '%02x%02x%02x%02x-%02x%02x-%02x%02x-%02x%02x-%02x%02x%02x%02x%02x%02x' % [
+	return '%02x%02x%02x%02x-%02x%02x-%02x%02x-%02x%02x-%02x%02x%02x%02x%02x%02x' % [
 	# low
 	b[0], b[1], b[2], b[3],
 
@@ -69,31 +69,31 @@ static func v4_rng(rng: RandomNumberGenerator):
 var _uuid: Array
 
 func _init(rng := RandomNumberGenerator.new()) -> void:
-  _uuid = uuidbinrng(rng)
+	_uuid = uuidbinrng(rng)
 
 func as_array() -> Array:
-  return _uuid.duplicate()
+	return _uuid.duplicate()
 
 func as_dict(big_endian := true) -> Dictionary:
-  if big_endian:
-    return {
-	  "low"  : (_uuid[0]  << 24) + (_uuid[1]  << 16) + (_uuid[2]  << 8 ) +  _uuid[3],
-	  "mid"  : (_uuid[4]  << 8 ) +  _uuid[5],
-	  "hi"   : (_uuid[6]  << 8 ) +  _uuid[7],
-	  "clock": (_uuid[8]  << 8 ) +  _uuid[9],
-	  "node" : (_uuid[10] << 40) + (_uuid[11] << 32) + (_uuid[12] << 24) + (_uuid[13] << 16) + (_uuid[14] << 8 ) +  _uuid[15]
+	if big_endian:
+		return {
+		  "low"  : (_uuid[0]  << 24) + (_uuid[1]  << 16) + (_uuid[2]  << 8 ) +  _uuid[3],
+		  "mid"  : (_uuid[4]  << 8 ) +  _uuid[5],
+		  "hi"   : (_uuid[6]  << 8 ) +  _uuid[7],
+		  "clock": (_uuid[8]  << 8 ) +  _uuid[9],
+		  "node" : (_uuid[10] << 40) + (_uuid[11] << 32) + (_uuid[12] << 24) + (_uuid[13] << 16) + (_uuid[14] << 8 ) +  _uuid[15]
 	}
-  else:
-    return {
-	  "low"  : _uuid[0]          + (_uuid[1]  << 8 ) + (_uuid[2]  << 16) + (_uuid[3]  << 24),
-	  "mid"  : _uuid[4]          + (_uuid[5]  << 8 ),
-	  "hi"   : _uuid[6]          + (_uuid[7]  << 8 ),
-	  "clock": _uuid[8]          + (_uuid[9]  << 8 ),
-	  "node" : _uuid[10]         + (_uuid[11] << 8 ) + (_uuid[12] << 16) + (_uuid[13] << 24) + (_uuid[14] << 32) + (_uuid[15] << 40)
-	}
-	
+	else:
+		return {
+		  "low"  : _uuid[0]          + (_uuid[1]  << 8 ) + (_uuid[2]  << 16) + (_uuid[3]  << 24),
+		  "mid"  : _uuid[4]          + (_uuid[5]  << 8 ),
+		  "hi"   : _uuid[6]          + (_uuid[7]  << 8 ),
+		  "clock": _uuid[8]          + (_uuid[9]  << 8 ),
+		  "node" : _uuid[10]         + (_uuid[11] << 8 ) + (_uuid[12] << 16) + (_uuid[13] << 24) + (_uuid[14] << 32) + (_uuid[15] << 40)
+		}
+
 func as_string() -> String:
-  return '%02x%02x%02x%02x-%02x%02x-%02x%02x-%02x%02x-%02x%02x%02x%02x%02x%02x' % [
+	return '%02x%02x%02x%02x-%02x%02x-%02x%02x-%02x%02x-%02x%02x%02x%02x%02x%02x' % [
 	# low
 	_uuid[0], _uuid[1], _uuid[2], _uuid[3],
 
@@ -108,9 +108,9 @@ func as_string() -> String:
 
 	# node
 	_uuid[10], _uuid[11], _uuid[12], _uuid[13], _uuid[14], _uuid[15]
-  ]
+	]
   
 func is_equal(other) -> bool:
   # Godot Engine compares Array recursively
   # There's no need for custom comparison here.
-  return _uuid == other._uuid
+	return _uuid == other._uuid
